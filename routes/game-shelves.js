1ca6dd2b8cc7ac62358e7a1456a464b505cd9bcc
Garrett Middleton <garrettmiddleton@gmail.com>
2021-09-22 19:38:25 -0700

    added GET route for book-shelves/add and shelfValidator

--- routes/game-shelves.js
+++ routes/game-shelves.js
@@ -1,19 +1,28 @@
 const express = require('express');
-const router = express.Router();
-
-const { asyncHandler } = require("./utils");
-
 const db = require("../db/models");
+const { csrfProtection, asyncHandler } = require("./utils");
+const { requireAuth } = require('../auth');
 const { Game, Game_Shelf, User } = db;
 
-router.get("/", asyncHandler(async (req, res, next) => {
+const router = express.Router();
+
+
+
+
+router.get("/", requireAuth, asyncHandler(async (req, res, next) => {
   console.log('You made it to the game shelves page.')
-  console.log(req.params)
-  const currentUser = await User.findByPk(req.params.id);
-  console.log(currentUser)
-  const shelves = await Game_Shelf.findAll()
-  res.render('game-shelves.pug', { currentUser, title: `Good Games`});
+  // console.log(Game.id)
+  // const currentUser = await db.User.findByPK({ order: [['user_name', 'ASC']] });
+  // // const currentUser = await User.findByPk(req.params.id);
+  // console.log(currentUser)
+  // // const shelves = await Game_Shelf.findAll()
+  // res.render('game-shelves.pug', { title: `Good Games`, currentUser });
   
+  const { userId } = req.session.auth
+  const user = await db.User.findByPk(userId)
+  console.log(userId)
+
+
   // if user has shelves created already, GET shelves and display them
 
 
@@ -24,6 +33,22 @@ router.get("/", asyncHandler(async (req, res, next) => {
 // add a shelf
   // GET to get the add a shelf form
   // POST to send it to db
+router.get('/book-shelves/add', requireAuth, csrfProtection, (req, res) => {
+  const book = db.Game_Shelf.build();
+  res.render('shelf-add.pug', {
+    title: 'Add Shelf',
+    book,
+    csrfToken: req.csrfToken(),
+  });
+});
+
+const shelfValidators = [
+  check('shelf_name')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Title')
+    .isLength({ max: 30 })
+    .withMessage('Shelf Name must not be more than 30 characters long'),
+];
 
 // update shelf
   // GET shelf id form
