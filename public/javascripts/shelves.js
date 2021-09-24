c56f6321b8a7eef0b36c397b4e41792f22db2f16
James Thompson <thompsonjames0120@gmail.com>
2021-09-24 00:18:58 -0500

    event listener is adding to database with no errors, can now manipulate DOM

--- public/javascripts/shelves.js
+++ public/javascripts/shelves.js
@@ -12,17 +12,26 @@ document.addEventListener("DOMContentLoaded", (e) => {
     e.preventDefault();
     console.log("----this event listener was hit-----");
 
-    const tester = document.createElement("h1")
+    const tester = document.createElement("div");
     // console.log(tester)
-    tester.innerText = "TESTER"
-    let body = document.querySelector(".shelf_list")
-    body.appendChild(tester)
-    await fetch("/api-game-shelves", {
-      method: "GET",
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-
-
+    tester.innerText = "TESTER";
+    let body = document.querySelector(".shelf_list");
+    body.appendChild(tester);
+    // await fetch("/api-game-shelves", {
+    //   method: "POST",
+    // })
+    // .then(response => response.json())
+    // .then(data => console.log(data))
+    const shelfName = document.getElementById("shelf_name")
+    console.log(shelfName.value)
+    let tasks = await fetch("/api-game-shelves", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({shelf_name: shelfName.value})
+    });
+    tasks = await tasks.json();
+    console.log("hit", tasks);
   });
 });
+
+// TODO: comment out if we cant get working and leave
