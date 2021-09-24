6317eb4fccd84aec84d3863cfa9a2dda1400d4b5
Concrete18 <michaelericson19@gmail.com>
2021-09-24 17:32:00 -0400

    stopped logging

--- config/database.js
+++ config/database.js
@@ -10,10 +10,13 @@ module.exports = {
     host,
     dialect: 'postgres',
     seederStorage: 'sequelize',
+    logging: false
   },
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
+    // change logging to true for showing sql executions again
+    logging: false
   }
 };
