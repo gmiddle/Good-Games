98d117d1e545e25146550cb24ce60fd75b2206e7
Concrete18 <michaelericson19@gmail.com>
2021-09-25 10:47:28 -0400

    added more to shelf entry seeder

--- db/seeders/20210921162037-seed_shelf_entry.js
+++ db/seeders/20210921162037-seed_shelf_entry.js
@@ -1,23 +1,32 @@
 'use strict';
 
+const games1 = [1, 2, 22, 25, 75, 82, 68, 89, 92, 95, 120, 150, 153]
+
+const games2 = [3, 4, 23, 26, 77, 88, 69, 80, 94, 96, 125, 154, 155]
+
+function createSeeder(games, gameShelfId) {
+  const arr = []
+  for (let gameId of games) {
+    const obj =   {
+      play_status: 'Playing',
+      gameId,
+      gameShelfId,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    arr.push(obj)
+  }
+  return arr
+}
+
+const seeder = [
+  ...createSeeder(games1, 1),
+  ...createSeeder(games2, 2)
+]
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-      return queryInterface.bulkInsert('Shelf_Entries', [
-      {
-        play_status: 'Playing',
-        gameId: '1',
-        gameShelfId: '1',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        play_status: 'Unplayed',
-        gameId: '2',
-        gameShelfId: '1',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ], {});
+      return queryInterface.bulkInsert('Shelf_Entries', seeder, {});
   },
 
   down: (queryInterface, Sequelize) => {
